7e9013162b12e3f70f220e48f7d5ac8c65c7a7d3
MinexAutomation <minex.automation@gmail.com>
2019-12-12 23:34:29 -0800

    Added helper methods to perform conditional logic.

---
+++ source/R5T.Heraklion/Code/Extensions/ICommandBuilderContextExtensions.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+using R5T.Magyar;
+
+
+namespace R5T.Heraklion
+{
+    public static class ICommandBuilderContextExtensions
+    {
+        public static string BuildCommand<TContext>(this ICommandBuilderContext<TContext> context)
+        {
+            var command = context.CommandBuilder.BuildCommand();
+            return command;
+        }
+
+        public static ICommandBuilderContext<TContext> Condition<TContext>(this ICommandBuilderContext<TContext> context, bool condition, Action ifTrue, Action ifFalse)
+        {
+            if(condition)
+            {
+                ifTrue();
+            }
+            else
+            {
+                ifFalse();
+            }
+
+            return context;
+        }
+
+        public static ICommandBuilderContext<TContext> Condition<TContext>(this ICommandBuilderContext<TContext> context, bool condition, Action ifTrue)
+        {
+            context.Condition(condition, ifTrue, ActionHelper.DoNothing);
+
+            return context;
+        }
+
+        public static ICommandBuilderContext<TContext> Condition<TContext>(this ICommandBuilderContext<TContext> context, bool condition, Action<ICommandBuilderContext<TContext>> ifTrue, Action<ICommandBuilderContext<TContext>> ifFalse)
+        {
+            if (condition)
+            {
+                ifTrue(context);
+            }
+            else
+            {
+                ifFalse(context);
+            }
+
+            return context;
+        }
+
+        public static ICommandBuilderContext<TContext> Condition<TContext>(this ICommandBuilderContext<TContext> context, bool condition, Action<ICommandBuilderContext<TContext>> ifTrue)
+        {
+            context.Condition(condition, ifTrue, ActionHelper.DoNothing);
+
+            return context;
+        }
+    }
+}

3856e065f79d19685d7518b79d24846cae88b391
MinexAutomation <minex.automation@gmail.com>
2019-12-17 16:44:28 -0800

    Added finished context and extensions.

---
+++ source/R5T.Heraklion/Code/Context Extensions/FinishedContextExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+namespace R5T.Heraklion.Extensions
+{
+    public static class FinishedContextExtensions
+    {
+        public static string Finish(this ICommandBuilderContext<FinishedContext> finishedContext)
+        {
+            var command = finishedContext.BuildCommand();
+            return command;
+        }
+    }
+}
---
+++ source/R5T.Heraklion/Code/Contexts/FinishedContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+
+namespace R5T.Heraklion
+{
+    /// <summary>
+    /// Context indicating that a command is finished.
+    /// </summary>
+    public class FinishedContext
+    {
+    }
+}

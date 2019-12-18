ae86c3e0b53f0bd5483c58b2aa258d586a3698f8
MinexAutomation <minex.automation@gmail.com>
2019-12-17 16:45:50 -0800

    Added command-builder context extensions that allow direct use of command-builder extensions without having to dereference the command-builder context's command-builder first.

--- source/R5T.Heraklion/Code/Extensions/ICommandBuilderContextExtensions.cs
+++ source/R5T.Heraklion/Code/Extensions/ICommandBuilderContextExtensions.cs
@@ -1,19 +1,66 @@
 ﻿using System;
 
 using R5T.Magyar;
+using R5T.Piraeus.Extensions;
 
 
-namespace R5T.Heraklion
+namespace R5T.Heraklion.Extensions
 {
     public static class ICommandBuilderContextExtensions
     {
+        public static ICommandBuilderContext<TContext> Append<TContext>(this ICommandBuilderContext<TContext> context, string token)
+        {
+            context.CommandBuilder.Append(token);
+
+            return context;
+        }
+
+        /// <summary>
+        /// Properly quotes a path token.
+        /// Surrounds the provided path with quotes in case of spaces in the path.
+        /// </summary>
+        public static ICommandBuilderContext<TContext> AppendPath<TContext>(this ICommandBuilderContext<TContext> context, string path)
+        {
+            context.CommandBuilder.AppendPath(path);
+
+            return context;
+        }
+
+        public static ICommandBuilderContext<TContext> AppendQuotedValue<TContext>(this ICommandBuilderContext<TContext> context, string value)
+        {
+            context.CommandBuilder.AppendQuotedValue(value);
+
+            return context;
+        }
+
+        public static ICommandBuilderContext<TContext> AppendNameValuePair<TContext>(this ICommandBuilderContext<TContext> context, string name, string value)
+        {
+            context.CommandBuilder.AppendNameValuePair(name, value);
+
+            return context;
+        }
+
+        public static ICommandBuilderContext<TContext> AppendNamePathValuePair<TContext>(this ICommandBuilderContext<TContext> context, string name, string pathValue)
+        {
+            context.CommandBuilder.AppendNamePathValuePair(name, pathValue);
+
+            return context;
+        }
+
+        public static ICommandBuilderContext<TContext> AppendNameQuotedValuePair<TContext>(this ICommandBuilderContext<TContext> context, string name, string value)
+        {
+            context.CommandBuilder.AppendNameQuotedValuePair(name, value);
+
+            return context;
+        }
+
         public static string BuildCommand(this ICommandBuilderContext context)
         {
             var command = context.CommandBuilder.BuildCommand();
             return command;
         }
 
-        public static ICommandBuilderContext Condition(this ICommandBuilderContext context, bool condition, Action ifTrue, Action ifFalse)
+        public static ICommandBuilderContext<TContext> Condition<TContext>(this ICommandBuilderContext<TContext> context, bool condition, Action ifTrue, Action ifFalse)
         {
             if(condition)
             {
@@ -27,7 +74,7 @@ namespace R5T.Heraklion
             return context;
         }
 
-        public static ICommandBuilderContext Condition(this ICommandBuilderContext context, bool condition, Action ifTrue)
+        public static ICommandBuilderContext<TContext> Condition<TContext>(this ICommandBuilderContext<TContext> context, bool condition, Action ifTrue)
         {
             context.Condition(condition, ifTrue, ActionHelper.DoNothing);
 
